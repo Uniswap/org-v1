6ce5b940c753910ed1d8bc4fdfed3e2fa9950d68
Callil Capuozzo <callil.capuozzo@gmail.com>
2020-03-23 16:49:44 -0400

    Design bugs

--- src/components/beta.js
+++ src/components/beta.js
@@ -4,27 +4,33 @@ import styled from 'styled-components'
 const StyledFooter = styled.p`
   position: fixed;
   right: 2rem;
-  bottom: 2rem;
+  bottom: 4px;
   font-size: 14px;
   font-style: italic;
   max-width: 200px;
-  opacity: 0.4;
   color: ${({ theme }) => theme.colors.link};
-  z-index: -99;
+  padding: 0.75rem;
+  border-radius: 12px 12px 0 0;
+  background-color: ${({ theme }) => theme.menuBG};
+  border: 1px solid ${({ theme }) => theme.colors.grey2};
+
+  z-index: 60;
+
   @media (max-width: 960px) {
     position: relative;
     width: 100%;
-    left: 1rem;
+    left: 0.5rem;
     bottom: 1rem;
+    border: none;
   }
 `
 
 const Footer = () => {
   return (
     <StyledFooter>
-      This site is a work in progress. Found an issue?{' '}
+      This site is a work in progress.{' '}
       <a href="https://github.com/Uniswap/uniswap-site-v2/issues">
-        <u>Get in touch.</u>
+        <u>Found an issue?</u>
       </a>
     </StyledFooter>
   )
