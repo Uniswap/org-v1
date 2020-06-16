9921ba1c6a6c549cfdb9bed11097759560cc001d
ianlapham <ianlapham@gmail.com>
2020-06-16 15:22:29 -0400

    update ticker with v2 stats

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -2,6 +2,11 @@ import ApolloClient from 'apollo-boost'
 import fetch from 'isomorphic-fetch'
 
 export const client = new ApolloClient({
-  uri: 'https://api.thegraph.com/subgraphs/name/graphprotocol/uniswap',
+  uri: 'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2',
+  fetch
+})
+
+export const blockClient = new ApolloClient({
+  uri: 'https://api.thegraph.com/subgraphs/name/blocklytics/ethereum-blocks',
   fetch
 })
