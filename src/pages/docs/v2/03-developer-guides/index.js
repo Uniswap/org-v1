4a6f7afaafc63b62cf6116f17bad0becbb285590
Callil Capuozzo <callil.capuozzo@gmail.com>
2020-06-22 13:30:19 -0400

    update terminology

--- src/pages/docs/v2/03-developer-guides/index.js
+++ src/pages/docs/v2/03-developer-guides/index.js
@@ -2,5 +2,5 @@ import React from 'react'
 import { Redirect } from '@reach/router'
 
 export default function Redirector({ path }) {
-  return <Redirect from={path} to={`${path}connect-to-uniswap/`} noThrow />
+  return <Redirect from={path} to={`${path}getting-started/`} noThrow />
 }
