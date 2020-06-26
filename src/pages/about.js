960e0672982226da0a78a3a90a90aafb78d350ee
ianlapham <ianlapham@gmail.com>
2020-06-26 11:41:23 -0700

    add link to other pages and blog posts

--- src/pages/about.js
+++ src/pages/about.js
@@ -123,7 +123,7 @@ const About = props => {
         </StyledSectionFlex>
         <StyledSectionFlex wrapSmall={false} style={{ paddingTop: '2rem' }}>
           <MiniCard
-            href="https://discord.gg/Y7TF6QA"
+            href="https://discord.gg/EwFs3Pp"
             title={'Discord'}
             image={data.discord.childImageSharp.fluid}
             desc={'Real time discussion.'}
